9d8dc6198d6c54154f2ad8d70d4311daff44556b
Lucas Martineau <lucasmartineau@yahoo.fr>
2016-11-22 11:17:50 +0100

    Third commit

---
+++ script_verboide.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ "$#" -eq 1 ]; then
+ filenameComplete=$1
+ filename="${filenameComplete%.*}"
+ fileToSave=$filename"_Occurences.txt"
+ echo "Sauvegardé sous le nom $fileToSave"
+ sed "s/[0-9.,:\#\;\!\(\)*\?\"\»]//g" $filenameComplete | sed "s/\[//g" | sed "s/\]//g" | tr " " "\012" | grep -v -w -f stopwords.txt | sort | sed '/^$/d' | uniq -c |sort -nb -r > $fileToSave
+fi
