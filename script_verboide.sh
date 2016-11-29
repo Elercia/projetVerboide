3e88e24a6438dd77ea7e133605e10ef2510b7d40
Vincent Rabier <rabierv@hotmail.fr>
2016-11-29 08:05:16 +0100

    Jai enleve le truc pour enlever les accents

--- script_verboide.sh
+++ script_verboide.sh
@@ -3,6 +3,6 @@ if [ "$#" -eq 1 ]; then
  filenameComplete=$1
  filename="${filenameComplete%.*}"
  fileToSave=$filename"_Occurences.txt"
- sed "s/[0-9.,:\#\;\!\(\)*\?\"\»]//g" $filenameComplete | sed "s/\[//g" | sed "s/\]//g" | tr " " "\012" | tr '[:upper:]' '[:lower:]' | iconv -f ISO-8859-1 -t ascii//TRANSLIT --verbose | grep -v -w -f stopwords.txt | sort | sed '/^$/d' | uniq -c |sort -nb -r > $fileToSave
+ sed "s/[0-9.,:\#\;\!\(\)*\?\"\»]//g" $filenameComplete | sed "s/\[//g" | sed "s/\]//g" | tr " " "\012" | tr '[:upper:]' '[:lower:]' | grep -v -w -f stopwords.txt | sort | sed '/^$/d' | uniq -c |sort -nb -r > $fileToSave
  echo "Sauvegardé sous le nom $fileToSave"
 fi
