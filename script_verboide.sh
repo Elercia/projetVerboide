7a3094dbb2979874c463319746af2cdf85ae6080
Vincent Rabier <rabierv@hotmail.fr>
2016-11-29 10:03:17 +0100

    lol

--- script_verboide.sh
+++ script_verboide.sh
@@ -3,7 +3,25 @@ if [ "$#" -eq 1 ]; then
 	filenameComplete=$1
 	filename="${filenameComplete%.*}"
 	fileToSave=$filename"_Occurences.txt"
-	sed "s/[0-9.,:\#\;\!\(\)*\?\"\»]//g" $filenameComplete | sed "s/\[//g" | sed "s/\]//g" | tr " " "\012" | tr '[:upper:]' '[:lower:]' | grep -v -w -f stopwords_fr.txt | sort | sed '/^$/d' | uniq -c |sort -nb -r > $fileToSave
+	#On enleve tous les caractères si dessous
+	sed "s/[0-9.,:\#\;\!\(\)*\?\"\»]//g" $filenameComplete |
+	sed "s/\[//g" | 
+	sed "s/\]//g" |
+	#remplace les espaces par les retour à la ligne 
+	tr " " "\012" | 
+	#mets le mots en minuscule
+	tr '[:upper:]' '[:lower:]' |
+	#on enleve les stop word fr et en (au cas ou) 
+	grep -v -w -f stopwords_fr.txt | 
+	grep -v -w -f stopwords_en.txt | 
+	#on tri (ché po)
+	sort | 
+	#enleve les lignes vides
+	sed '/^$/d' |
+	#compte les mots
+	uniq -c |
+	#on tri par nb d'occurence et on save
+	sort -nb -r > $fileToSave
 	echo "Sauvegardé sous le nom $fileToSave"
 else
 	echo "Usage ./script_verboide.sh <file_to_check>"
