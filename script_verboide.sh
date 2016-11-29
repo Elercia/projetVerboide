adfaabf9518e4faedbaf648b69a19366a2f4778b
Vincent Rabier <rabierv@hotmail.fr>
2016-11-29 09:55:39 +0100

    ajout stop words fr en

--- script_verboide.sh
+++ script_verboide.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 if [ "$#" -eq 1 ]; then
- filenameComplete=$1
- filename="${filenameComplete%.*}"
- fileToSave=$filename"_Occurences.txt"
- sed "s/[0-9.,:\#\;\!\(\)*\?\"\»]//g" $filenameComplete | sed "s/\[//g" | sed "s/\]//g" | tr " " "\012" | tr '[:upper:]' '[:lower:]' | grep -v -w -f stopwords.txt | sort | sed '/^$/d' | uniq -c |sort -nb -r > $fileToSave
- echo "Sauvegardé sous le nom $fileToSave"
+	filenameComplete=$1
+	filename="${filenameComplete%.*}"
+	fileToSave=$filename"_Occurences.txt"
+	sed "s/[0-9.,:\#\;\!\(\)*\?\"\»]//g" $filenameComplete | sed "s/\[//g" | sed "s/\]//g" | tr " " "\012" | tr '[:upper:]' '[:lower:]' | grep -v -w -f stopwords_fr.txt | sort | sed '/^$/d' | uniq -c |sort -nb -r > $fileToSave
+	echo "Sauvegardé sous le nom $fileToSave"
+else
+	echo "Usage ./script_verboide.sh <file_to_check>"
 fi
