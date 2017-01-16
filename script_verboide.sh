33d74099455b665ee0ba8f66af23d3b398d38dc5
Vincent Rabier <rabierv@hotmail.fr>
2017-01-16 09:22:13 +0100

    Upgrades trop bien

--- script_verboide.sh
+++ script_verboide.sh
@@ -21,16 +21,55 @@ if [ "$#" -eq 1 ]; then
 	#on enleve les stop word fr et en (au cas ou) 
 	fgrep -v -w -f Stopwords/stopwords_fr.txt |
 	fgrep -v -w -f Stopwords/stopwords_en.txt |
+	fgrep -v -w -f Stopwords/stopwords_es.txt |
+	fgrep -v -w -f Stopwords/stopwords_de.txt |
+	fgrep -v -w -f Stopwords/stopwords_it.txt |
+	fgrep -v -w -f Stopwords/stopwords_ru.txt |
 	#on tri (ché po)
 	sort |
 	#enleve les lignes vides
 	sed '/^$/d' |
 	#enleve les mots de 1 lettre
-	sed '/^.$/d' |
+	sed '/^.$/d' > tmp
+
+	content=$(tr '[:space:]' '\n' < tmp)
+	tr '[:space:]' '\n' < tmp > fileContent
+
+	#on cherche le nombre d'occurence le plus elevé
+	langueArray=("fr" "en" "es" "de" "it" "ru")
+	langueReconnue=""
+	occurenceMax=-1
+	for i in "${langueArray[@]}"
+	do
+		#comparer avec Samples/sample$i.txt
+		#recup result de comm
+		#si plus grand que ancien plus grand remplacer
+		#ramplacer la var de langue
+		#afficher
+		fileToRead=$(cat "Samples/sample_$i.txt")
+		currentOccu=comm -12 fileContent $fileToRead | wc -l
+		echo $currentOccu
+		if [ currentOccu > occurenceMax ]
+		then
+		        occurenceMax=currentOccu
+		        langueReconnue=$i
+		fi
+	done
+	echo "La langue reconnue est : $langueReconnue"
+
+
+
+
+
 	#compte les mots
-	uniq -c |
+	#uniq -c |
 	#on tri par nb d'occurence et on save
-	sort -nb -r > $fileToSave
+	#sort -nb -r > $fileToSave
+	
+	# echo comm Samples/sample_fr fileContent
+	#On suppime les ficier temporaire que l'on a utilisé
+	rm tmp
+	rm fileContent
 	echo "Sauvegardé sous le nom $fileToSave"
 else
 	echo "Usage ./script_verboide.sh <file_to_check>"
